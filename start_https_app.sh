c81b7a561ed66c6026a79c94aca09e7f366d5345
u-masao <4973920+u-masao@users.noreply.github.com>
2022-02-21 10:53:12 +0900

    add application support HTTPS

---
+++ start_https_app.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+openssl genrsa -aes256 -passout pass:gsahdg -out server.pass.key 4096
+openssl rsa -passin pass:gsahdg -in server.pass.key -out server.key
+openssl req -new -key server.key -out server.csr -subj "/CN=localhost"
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+rm server.pass.key server.csr
+echo -e "import streamlit as st\nst.title('hello')" > app.py
+streamlit run app.py --server.sslCertFile=server.crt --server.sslKeyFile=server.key --server.address=localhost
